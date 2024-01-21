d5d835ef3ad164177a0e61a1113790ccff7f9b10
axin668 <axin99966@163.com>
2024-01-21 17:25:37 +0800

    feat: 检查用户名是否存在(用于注册时判断)

--- admin/src/main/java/com/axinstar/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/com/axinstar/shortlink/admin/controller/UserController.java
@@ -1,12 +1,15 @@
 package com.axinstar.shortlink.admin.controller;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.axinstar.shortlink.admin.common.convention.result.Result;
 import com.axinstar.shortlink.admin.common.convention.result.Results;
+import com.axinstar.shortlink.admin.dto.resp.UserActualRespDTO;
 import com.axinstar.shortlink.admin.dto.resp.UserRespDTO;
 import com.axinstar.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -24,5 +27,18 @@ public class UserController {
     @GetMapping("/api/shortlink/v1/user/{username}")
     public Result<UserRespDTO> getUserByUsername(@PathVariable("username") String username) {
         return Results.success(userService.getUserByUsername(username));
+    }
+
+    /**
+     * 根据用户名查询用户无脱敏信息
+     */
+    @GetMapping("/api/shortlink/v1/actual/user/{username}")
+    public Result<UserActualRespDTO> getActualUserByUsername(@PathVariable("username") String username) {
+        return Results.success(BeanUtil.toBean(userService.getUserByUsername(username), UserActualRespDTO.class));
+    }
+
+    @GetMapping("/api/shortlink/v1/user/has-username")
+    public Result<Boolean> hasUsername(@RequestParam("username") String username) {
+        return Results.success(userService.hasUsername(username));
     }
 }
