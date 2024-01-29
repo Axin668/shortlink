e755469ec1c84121fcb8c6dd230deabf2b2385a2
axin668 <axin99966@163.com>
2024-01-30 00:38:45 +0800

    feat: 开发短链接后管系统-短链接分组绑定用户上下文

--- admin/src/main/java/com/axinstar/shortlink/admin/service/impl/GroupServiceImpl.java
+++ admin/src/main/java/com/axinstar/shortlink/admin/service/impl/GroupServiceImpl.java
@@ -1,6 +1,7 @@
 package com.axinstar.shortlink.admin.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
+import com.axinstar.shortlink.admin.common.biz.user.UserContext;
 import com.axinstar.shortlink.admin.dao.entity.GroupDO;
 import com.axinstar.shortlink.admin.dao.mapper.GroupMapper;
 import com.axinstar.shortlink.admin.dto.resp.ShortLinkGroupRespDTO;
@@ -30,6 +31,7 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
         GroupDO groupDO = GroupDO.builder()
                 .gid(gid)
                 .sortOrder(0)
+                .username(UserContext.getUsername())
                 .name(groupName)
                 .build();
         baseMapper.insert(groupDO);
@@ -41,7 +43,7 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
         LambdaQueryWrapper<GroupDO> queryWrapper = Wrappers.lambdaQuery(GroupDO.class)
                 .eq(GroupDO::getDelFlag, 0)
                 // TODO 从当前上下文获取用户名
-                .eq(GroupDO::getUsername, "mading")
+                .eq(GroupDO::getUsername, UserContext.getUsername())
                 .orderByDesc(GroupDO::getSortOrder, GroupDO::getUpdateTime);
         List<GroupDO> groupDOList = baseMapper.selectList(queryWrapper);
         return BeanUtil.copyToList(groupDOList, ShortLinkGroupRespDTO.class);
@@ -51,7 +53,7 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
         LambdaQueryWrapper<GroupDO> queryWrapper = Wrappers.lambdaQuery(GroupDO.class)
                 .eq(GroupDO::getGid, gid)
                 // TODO 设置用户名
-                .eq(GroupDO::getUsername, null)
+                .eq(GroupDO::getUsername, UserContext.getUsername())
                 .eq(GroupDO::getDelFlag, 0);
         GroupDO hasGroupFlag = baseMapper.selectOne(queryWrapper);
         return hasGroupFlag != null;
