3f6ed05d558f96bea4349b4080f72f8d7e9d88e2
axin668 <axin99966@163.com>
2024-02-02 00:19:42 +0800

    refactor: 重构短链接后管系统基础路径

--- admin/src/main/java/com/axinstar/shortlink/admin/controller/GroupController.java
+++ admin/src/main/java/com/axinstar/shortlink/admin/controller/GroupController.java
@@ -24,7 +24,7 @@ public class GroupController {
     /**
      * 新增短链接分组
      */
-    @PostMapping("/api/short-link/v1/group")
+    @PostMapping("/api/short-link/admin/v1/group")
     public Result<Void> save(@RequestBody ShortLinkGroupSaveReqDTO requestParam) {
         groupService.saveGroup(requestParam.getName());
         return Results.success();
@@ -33,7 +33,7 @@ public class GroupController {
     /**
      * 查询短链接分组集合
      */
-    @GetMapping("/api/short-link/v1/group")
+    @GetMapping("/api/short-link/admin/v1/group")
     public Result<List<ShortLinkGroupRespDTO>> listGroup() {
         List<ShortLinkGroupRespDTO> listGroup = groupService.listGroup();
         return Results.success(listGroup);
@@ -42,7 +42,7 @@ public class GroupController {
     /**
      * 修改短链接分组名
      */
-    @PutMapping("/api/short-link/v1/group")
+    @PutMapping("/api/short-link/admin/v1/group")
     public Result<Void> updateGroup(@RequestBody ShortLinkGroupUpdateReqDTO requestParam) {
         groupService.updateGroup(requestParam);
         return Results.success();
@@ -51,7 +51,7 @@ public class GroupController {
     /**
      * 删除短链接分组名
      */
-    @DeleteMapping("/api/short-link/v1/group")
+    @DeleteMapping("/api/short-link/admin/v1/group")
     public Result<Void> deleteGroup(@RequestParam String gid) {
         groupService.deleteGroup(gid);
         return Results.success();
@@ -60,7 +60,7 @@ public class GroupController {
     /**
      * 短链接分组排序
      */
-    @PostMapping("/api/short-link/v1/group/sort")
+    @PostMapping("/api/short-link/admin/v1/group/sort")
     public Result<Void> sortGroup(@RequestBody List<ShortLinkGroupSortReqDTO> requestParam) {
         groupService.sortGroup(requestParam);
         return Results.success();
