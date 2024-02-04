8abcb4144d05f7de616804a8d56479ded9717e14
axin668 <axin99966@163.com>
2024-02-04 22:01:26 +0800

    optimize: 短链接跳转原始链接功能-解决缓存击穿问题

---
+++ project/src/main/java/com/axinstar/shortlink/project/common/constant/RedisKeyConstant.java
@@ -0,0 +1,17 @@
+package com.axinstar.shortlink.project.common.constant;
+
+/**
+ * Redis Key 常量类
+ */
+public class RedisKeyConstant {
+
+    /**
+     * 短链接跳转前缀 Key
+     */
+    public static final String GOTO_SHORT_LINK_KEY = "short-link_goto_%s";
+
+    /**
+     * 短链接跳转分布式锁前缀 Key
+     */
+    public static final String LOCK_GOTO_SHORT_LINK_KEY = "short-link_lock_goto_%s";
+}
