458761c4947a853c6fcdcf4d25c3e591d8e9295c
axin668 <axin99966@163.com>
2024-02-07 00:11:51 +0800

    feat: 开发短链接监控之地区访问数据

---
+++ project/src/main/java/com/axinstar/shortlink/project/dao/mapper/LinkLocaleStatsMapper.java
@@ -0,0 +1,48 @@
+package com.axinstar.shortlink.project.dao.mapper;
+
+import com.axinstar.shortlink.project.dao.entity.LinkLocaleStatsDO;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * 地区统计访问持久层
+ */
+@Mapper
+public interface LinkLocaleStatsMapper extends BaseMapper<LinkLocaleStatsDO> {
+
+    /**
+     * 记录地区访问监控数据
+     */
+    @Insert("INSERT INTO\n" +
+            "  t_link_locale_stats (\n" +
+            "    full_short_url,\n" +
+            "    gid,\n" +
+            "    date,\n" +
+            "    cnt,\n" +
+            "    province,\n" +
+            "    city,\n" +
+            "    adcode,\n" +
+            "    country,\n" +
+            "    create_time,\n" +
+            "    update_time,\n" +
+            "    del_flag\n" +
+            "  )\n" +
+            "VALUES(\n" +
+            "    #{linkLocaleStats.fullShortUrl},\n" +
+            "    #{linkLocaleStats.gid},\n" +
+            "    #{linkLocaleStats.date},\n" +
+            "    #{linkLocaleStats.cnt},\n" +
+            "    #{linkLocaleStats.province},\n" +
+            "    #{linkLocaleStats.city},\n" +
+            "    #{linkLocaleStats.adcode},\n" +
+            "    #{linkLocaleStats.country},\n" +
+            "    NOW(),\n" +
+            "    NOW(),\n" +
+            "    0\n" +
+            "  ) ON DUPLICATE KEY\n" +
+            "UPDATE\n" +
+            "  cnt = cnt + #{linkLocaleStats.cnt};")
+    void shortLinkLocaleStats(@Param("linkLocaleStats") LinkLocaleStatsDO linkLocaleStatsDO);
+}
--- project/src/main/java/com/axinstar/shortlink/project/service/impl/ShortLinkServiceImpl.java
+++ project/src/main/java/com/axinstar/shortlink/project/service/impl/ShortLinkServiceImpl.java
@@ -7,13 +7,18 @@ import cn.hutool.core.lang.UUID;
 import cn.hutool.core.text.StrBuilder;
 import cn.hutool.core.util.ArrayUtil;
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.http.HttpUtil;
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
 import com.axinstar.shortlink.project.common.convention.exception.ClientException;
 import com.axinstar.shortlink.project.common.convention.exception.ServiceException;
 import com.axinstar.shortlink.project.common.enums.ValidateTypeEnum;
 import com.axinstar.shortlink.project.dao.entity.LinkAccessStatsDO;
+import com.axinstar.shortlink.project.dao.entity.LinkLocaleStatsDO;
 import com.axinstar.shortlink.project.dao.entity.ShortLinkDO;
 import com.axinstar.shortlink.project.dao.entity.ShortLinkGotoDO;
 import com.axinstar.shortlink.project.dao.mapper.LinkAccessStatsMapper;
+import com.axinstar.shortlink.project.dao.mapper.LinkLocaleStatsMapper;
 import com.axinstar.shortlink.project.dao.mapper.ShortLinkGotoMapper;
 import com.axinstar.shortlink.project.dao.mapper.ShortLinkMapper;
 import com.axinstar.shortlink.project.dto.req.ShortLinkCreateReqDTO;
@@ -43,6 +48,7 @@ import org.jsoup.nodes.Element;
 import org.redisson.api.RBloomFilter;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
@@ -55,6 +61,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import static com.axinstar.shortlink.project.common.constant.RedisKeyConstant.*;
+import static com.axinstar.shortlink.project.common.constant.ShortLinkConstant.AMAP_REMOTE_URL;
 
 /**
  * 短链接接口实现层
@@ -69,6 +76,10 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
     private final StringRedisTemplate stringRedisTemplate;
     private final RedissonClient redissonClient;
     private final LinkAccessStatsMapper linkAccessStatsMapper;
+    private final LinkLocaleStatsMapper linkLocaleStatsMapper;
+
+    @Value("${short-link.stats.locale.amap-key}")
+    private String statsLocaleAmapKey;
 
     @Override
     public ShortLinkCreateRespDTO createShortLink(ShortLinkCreateReqDTO requestParam) {
@@ -320,6 +331,29 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
                     .date(new Date())
                     .build();
             linkAccessStatsMapper.shortLinkStats(linkAccessStatsDO);
+            Map<String, Object> localeParamMap = new HashMap<>();
+            localeParamMap.put("key", statsLocaleAmapKey);
+            localeParamMap.put("ip", remoteAddr);
+            String localeResultStr = HttpUtil.get(AMAP_REMOTE_URL, localeParamMap);
+            JSONObject localeResultObj = JSON.parseObject(localeResultStr);
+            String infoCode = localeResultObj.getString("infocode");
+            LinkLocaleStatsDO linkLocaleStatsDO;
+            if (StrUtil.isNotBlank(infoCode) && StrUtil.equals(infoCode, "10000")) {
+                // 这里默认认为: 一个地区字段有值, 其它也有值
+                String province = localeResultObj.getString("province");
+                boolean unknownFlag = StrUtil.equals(province, "[]");
+                linkLocaleStatsDO = LinkLocaleStatsDO.builder()
+                        .fullShortUrl(fullShortUrl)
+                        .gid(gid)
+                        .date(new Date())
+                        .province(unknownFlag ? "未知" : province)
+                        .city(unknownFlag ? "未知" : localeResultObj.getString("city"))
+                        .adcode(unknownFlag ? "未知" : localeResultObj.getString("adcode"))
+                        .country("中国")
+                        .cnt(1)
+                        .build();
+                linkLocaleStatsMapper.shortLinkLocaleStats(linkLocaleStatsDO);
+            }
         } catch (Exception ex) {
             log.error("短链接访问量统计异常", ex);
         }
