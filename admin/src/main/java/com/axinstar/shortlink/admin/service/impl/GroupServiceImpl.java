dc0bf8cc6f1ca136509c0ce6a9b0a3db33f2c439
axin668 <axin99966@163.com>
2024-02-13 12:14:16 +0800

    optimize: 优化单个用户创建分组数量不能大于20

--- admin/src/main/java/com/axinstar/shortlink/admin/service/impl/GroupServiceImpl.java
+++ admin/src/main/java/com/axinstar/shortlink/admin/service/impl/GroupServiceImpl.java
@@ -2,6 +2,7 @@ package com.axinstar.shortlink.admin.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
 import com.axinstar.shortlink.admin.common.biz.user.UserContext;
+import com.axinstar.shortlink.admin.common.convention.exception.ClientException;
 import com.axinstar.shortlink.admin.common.convention.result.Result;
 import com.axinstar.shortlink.admin.dao.entity.GroupDO;
 import com.axinstar.shortlink.admin.dao.mapper.GroupMapper;
@@ -16,19 +17,31 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
+import static com.axinstar.shortlink.admin.common.constant.RedisCacheConstant.LOCK_GROUP_CREATE_KEY;
+
 /**
  * 短链接分组接口实现层
  */
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implements GroupService {
+
+    private final RedissonClient redissonClient;
+
+    @Value("${short-link.group.max-num}")
+    private Integer groupMaxNum;
 
     /**
      * 后续重构为 SpringCloud Feign 调用
@@ -43,17 +56,30 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
 
     @Override
     public void saveGroup(String username, String groupName) {
-        String gid;
-        do {
-            gid = RandomGenerator.generateRandom();
-        } while (hasGid(username, gid));
-        GroupDO groupDO = GroupDO.builder()
-                .gid(gid)
-                .sortOrder(0)
-                .username(username)
-                .name(groupName)
-                .build();
-        baseMapper.insert(groupDO);
+        RLock lock = redissonClient.getLock(String.format(LOCK_GROUP_CREATE_KEY, username));
+        lock.lock();
+        try {
+            LambdaQueryWrapper<GroupDO> queryWrapper = Wrappers.lambdaQuery(GroupDO.class)
+                    .eq(GroupDO::getUsername, username)
+                    .eq(GroupDO::getDelFlag, 0);
+            List<GroupDO> groupDOList = baseMapper.selectList(queryWrapper);
+            if (groupDOList != null && groupDOList.size() == groupMaxNum) {
+                throw new ClientException(String.format("已超出最大分组数: %d", groupMaxNum));
+            }
+            String gid;
+            do {
+                gid = RandomGenerator.generateRandom();
+            } while (hasGid(username, gid));
+            GroupDO groupDO = GroupDO.builder()
+                    .gid(gid)
+                    .sortOrder(0)
+                    .username(username)
+                    .name(groupName)
+                    .build();
+            baseMapper.insert(groupDO);
+        } finally {
+            lock.unlock();
+        }
     }
 
     @Override
