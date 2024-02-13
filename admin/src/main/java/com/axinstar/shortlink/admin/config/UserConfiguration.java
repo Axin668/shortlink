1ece1d3ca63f732ee7c302fc67dfd1dfe8cc45c2
axin668 <axin99966@163.com>
2024-02-13 17:11:08 +0800

    feat: 短链接后管通过风控保障系统稳定性

--- admin/src/main/java/com/axinstar/shortlink/admin/config/UserConfiguration.java
+++ admin/src/main/java/com/axinstar/shortlink/admin/config/UserConfiguration.java
@@ -1,6 +1,8 @@
 package com.axinstar.shortlink.admin.config;
 
+import com.axinstar.shortlink.admin.common.biz.user.UserFlowRiskControlFilter;
 import com.axinstar.shortlink.admin.common.biz.user.UserTransmitFilter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,6 +23,18 @@ public class UserConfiguration {
         registration.setFilter(new UserTransmitFilter(stringRedisTemplate));
         registration.addUrlPatterns("/*");
         registration.setOrder(0);
+        return registration;
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "short-link.flow-limit.enable", havingValue = "true")
+    public FilterRegistrationBean<UserFlowRiskControlFilter> globalUserFlowRiskControlFilter(
+            StringRedisTemplate stringRedisTemplate,
+            UserFlowRiskControlConfiguration userFlowRiskControlConfiguration) {
+        FilterRegistrationBean<UserFlowRiskControlFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new UserFlowRiskControlFilter(stringRedisTemplate, userFlowRiskControlConfiguration));
+        registration.addUrlPatterns("/*");
+        registration.setOrder(10);
         return registration;
     }
 }
